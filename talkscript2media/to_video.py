3498274e2bcaac007a76fb6363ce8d8d369121dd
manuela98 <manujarare@hotmail.com>
2018-01-22 19:29:17 -0500

    Modifying routes and generalizing for images

--- talkscript2media/to_video.py
+++ talkscript2media/to_video.py
@@ -1,7 +1,9 @@
 import shlex, subprocess
 import re
 import os 
-#import text_to_audio
+
+
+
 
 
 def Image_Video():
@@ -9,33 +11,28 @@ def Image_Video():
 	command_line ='ffmpeg -i Duration.txt  -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"  Video0.mp4'
 	args = shlex.split(command_line)	
 	subprocess.Popen(args).communicate()
-
-
 Image_Video()
 
-#N
-def U_Audios():
+def U_Audios(N,route_audio):
 #Union of audios
 	f= open('Aud.txt','w') 
-	path, dirs, files = os.walk("tests/img").__next__()
-	N = len(files)
-	for i in range(1,N+1):
-	    f.write('file tests/audio/0%d.wav \n'%i)
+	for i in range(N):
+	    f.write('file '+route_audio+'/0%d.wav \n'%i)
 	f.close()
-	command_line ='ffmpeg -f concat -safe 0 -i Aud.txt -c copy tests/audio/audio.wav'
+	command_line ='ffmpeg -f concat -safe 0 -i Aud.txt -c copy '+route_audio+'/audio.wav'
 	args = shlex.split(command_line)
 	subprocess.Popen(args).communicate()
 
-U_Audios()
+
 
 #Concatenate Video with Audio
 
-def Video_U_Audio():
-	command_line ='ffmpeg -i Video0.mp4 -i tests/audio/audio.wav -strict -2 Video00.mp4'
+def Video_U_Audio(route_audio,route_video,name_video):
+	command_line ='ffmpeg -i Video0.mp4 -i '+route_audio+'/audio.wav -strict -2 '+route_video+'/'+name_video
 	args = shlex.split(command_line)
 	subprocess.Popen(args).communicate()
 
-Video_U_Audio()
+
 
 
 

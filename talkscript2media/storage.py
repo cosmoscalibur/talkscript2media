8443849deedbdb48a49f81c6286489afd54dd4a6
manuela98 <manujarare@hotmail.com>
2018-01-22 19:20:06 -0500

    Modifying routes

--- talkscript2media/storage.py
+++ talkscript2media/storage.py
@@ -2,14 +2,54 @@
 
 import os
 
-def storage():
+def Storage(route_images,route_text,route_audio,route_video):
 
-	Ruta = [r'tests/audio',r'tests/img',r'tests/text']
+    
+    #image storage path,text storage path
+    
+    if not os.path.exists(route_images) or not os.path.exists(route_text) or not os.path.exists(route_video):
+        print('the routes do not exist')
+    
+    else: 
+     #if they extend, a route is created to store the audio
+       if not os.path.exists(route_audio): os.makedirs(route_audio)
+            
+    return 0
+            
+
+#verifying the existence of each file
 
 
-	for i in Ruta:
+def Existence(file,route_img,route_text):
+    
+    f = open(file,'r')
+    h = f.readlines()
+    language = h[0]
+    
+    img = []
+    text = []
+    
+    for i in range(1,len(h)-1):
+        img+=[h[i].split('>')[0]]
+        text+=[h[i].split('>')[1].split('\n')[0]]
+        
+    for i in range(len(img)):
+        
+        if os.path.isfile(route_img+'/'+img[i]) and  os.path.isfile(route_text+'/'+text[i]):
+            print ('exist')
+        else:
+            print('the files not  exist')
+    
+    return img,text,language.split('\n')[0]
+    
 
-		if not os.path.exists(i): os.makedirs(i)
+
+        
+        
+        
+        
+        
+        
 
 
-storage()
+

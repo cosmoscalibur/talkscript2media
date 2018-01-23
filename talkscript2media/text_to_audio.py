ebffb6c9b8056ac813c777a401807ef0503d1678
manuela98 <manujarare@hotmail.com>
2018-01-22 19:25:12 -0500

    Modifying routes and eliminating double execution of Duration

--- talkscript2media/text_to_audio.py
+++ talkscript2media/text_to_audio.py
@@ -2,38 +2,41 @@ import shlex, subprocess
 import re
 import os
 
-def Number_images():
+def Number_images(route_images):
 	
-	path, dirs, files = os.walk("tests/img").__next__()
+	path, dirs, files = os.walk(route_images).__next__()
 	N = len(files)
 
 	return N
 
 
 
-def Audio_festival():
+
+def Audio_festival(route_images,route_text,route_audio,text,language):
+  
  	#from text generating audio files with festival 
-	for i in range(1,Number_images()+1):
-		command_line = 'text2wave tests/text/0%d.txt'%i + ' -o tests/audio/0%d.wav'%i
+	for i in range(Number_images(route_images)):
+		command_line = 'text2wave '+route_text+'/'+text[i]+' -o '+route_audio+'/0%d.wav'%i #language !!
+         
 		args = shlex.split(command_line)
 		subprocess.Popen(args).communicate()
 
 
-Audio_festival()
 
-def Duration():
+
+
+def Duration(route_images,route_audio,img):
+    
 
 	f= open('Duration.txt','w')
 	f.write('ffconcat version 1.0\n')
-	for i in range(1,Number_images()+1):
-    		f.write('file  tests/img/0%d.jpg \n'%i)
-    		process = subprocess.Popen(['ffmpeg',  '-i','tests/audio/0%d.wav'%i], stdout=subprocess.PIPE, 			stderr=subprocess.STDOUT)
+	for i in range(Number_images(route_images)):
+    		f.write('file '+route_images+'/'+img[i]+' \n')
+    		process = subprocess.Popen(['ffmpeg',  '-i',route_audio+'/0%d.wav'%i], stdout=subprocess.PIPE, 			stderr=subprocess.STDOUT)
     		out,_ = process.communicate()
     		result = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),",out.decode	('utf-8'), re.DOTALL).groupdict()
     		f.write('duration  '+result['seconds']+'\n')
 	f.close()
 
-Duration()
 
-Duration()
 

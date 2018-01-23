50242cc53b6c3d3924e2bd83fb089b049c2db596
Edward Villegas <cosmoscalibur@gmail.com>
2018-01-23 15:59:58 -0500

    Fixed call to Number_images function.

--- talkscript2media/__main__.py
+++ talkscript2media/__main__.py
@@ -11,7 +11,7 @@ route_text = 'tests/text'
 route_audio = 'tests/audio'
 
 
-N = Number_images(route_images)
+N = text_to_audio.Number_images(route_images)
 
 storage.Storage(route_images,route_text,route_audio,route_output_video )
 
@@ -20,10 +20,10 @@ storage.Existence(file_name,route_images,route_text)
 
 Exist = storage.Existence(file_name,route_images,route_text)
 
-text_to_audio.Audio_festival(route_text,route_audio,Exist[1],Exist[2],N) #Exist[1] Name texts ,Exist[2] language 
+text_to_audio.Audio_festival(route_text,route_audio,Exist[1],Exist[2],N) #Exist[1] Name texts ,Exist[2] language
 
 
-text_to_audio.Duration(N,route_images,route_audio,Exist[0])# Exist[0] Name images 
+text_to_audio.Duration(N,route_images,route_audio,Exist[0])# Exist[0] Name images
 
 to_video.Image_Video()
 

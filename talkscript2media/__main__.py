a989cd6368312ec78275a5b8d0bc872e67018259
Edward Villegas <cosmoscalibur@gmail.com>
2018-01-22 20:21:53 -0500

    Absolute import for packaging when use python 3.

--- talkscript2media/__main__.py
+++ talkscript2media/__main__.py
@@ -1,6 +1,6 @@
-import storage
-import text_to_audio
-import to_video
+import talkscript2media.storage as storage
+import talkscript2media.text_to_audio as text_to_audio
+import talkscript2media.to_video as to_video
 
 file_name = 'file.txt'
 route_output_video = 'tests'
@@ -17,7 +17,7 @@ storage.Storage(route_images,route_text,route_audio,route_output_video )
 storage.Existence(file_name,route_images,route_text)
 
 text_to_audio.Audio_festival(route_images,route_text,route_audio,storage.Existence(file_name,route_images,route_text)[1],storage.Existence(file_name,route_images,route_text)[2])#text
-                             
+
                              #Audio_festival('tests/img','tests/text','tests/audio',['01.txt','02.txt','03.txt'],'en')
 
 text_to_audio.Duration(route_images,route_audio,storage.Existence(file_name,route_images,route_text)[0])#img
@@ -27,7 +27,3 @@ to_video.Image_Video()
 to_video.U_Audios(text_to_audio.Number_images(route_images),route_audio)
 
 to_video.Video_U_Audio(route_audio,route_output_video ,name_output_video)
-
-
-
-

41a8bb0f3570cbea6fe048e5b9154be1ec58028b
manuela98 <manujarare@hotmail.com>
2018-01-31 19:51:39 -0500

    Adding console output to file

--- talkscript2media/text_to_audio.py
+++ talkscript2media/text_to_audio.py
@@ -4,19 +4,29 @@ import re
 import os
 
 def Number_images(route_images):
+    
     path, dirs, files = os.walk(route_images).__next__()
     N = len(files)
     return N
 
 
 def Audio_festival(route_text, route_audio, text, language, N):
-    #from text generating audio files with festival
+    
+    # From text generating audio files with festival
+    
+    log = open('process.log', 'a')
+    log.write('Audio_festival')
+    log.flush()
+    print('Text to Audio....')
+    print(language)
+
     for i in range(N):
         command_line = 'text2wave '+route_text + '/' + text[i] + ' -o ' \
-            + route_audio + '/0%d.wav'%i #language !!
+        + route_audio + '/0%d.wav '%i #language !!
         args = shlex.split(command_line)
-        subprocess.Popen(args).communicate()
-
+        c = subprocess.Popen(args, stdout=log, stderr=log)
+        
+        
 
 def Duration(N, route_images, route_audio, img):
     #generates a file with the duration of each audio, with the format specified by ffmpeg

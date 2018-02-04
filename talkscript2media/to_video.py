13aa2712c1ca64476a489d3c016b50fdafa2632a
manuela98 <manujarare@hotmail.com>
2018-02-04 11:12:05 -0500

    Adding documentation y rename functions

--- talkscript2media/to_video.py
+++ talkscript2media/to_video.py
@@ -3,45 +3,69 @@ import subprocess
 import time
 
 
-def Image_Video(r_video):
+def image_video(r_video):
+    """Generating video from images and archive duration
+    with format specifited by ffmpeg.
     
-    # Generating video from images and archive duration with ffmpeg
+    Args:
+    r_video (str): Route of video.
     
+    Returns:
+    Video0.mp4 (video): Video of images each with specific duration.
     
-    log = open('process.log', 'a')
-    log.write('Image_Video\n')
+    """
+    log = open('process.log', 'a') # File log 
+    log.write('image_video\n')
     log.flush()  
     print('Image to video...')
+    
+    
     command_line = 'ffmpeg -i Duration.txt -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"  '+r_video+'/Video0.mp4'
     args = shlex.split(command_line)
     c = subprocess.Popen(args, stdout=log, stderr=log)
 
 
-def U_Audios(N,route_audio):
+def u_audios(N,route_audio):
+    """Concatenate audios.
     
+    Args:
+    N (int): Number of images.
+    route_audio (str): Route of audio files.
+    
+    Returns:
+    audio.wav (file): Audio union.
+    Aud.txt (file): Archivo for  union of audios with ffmpeg.
+    
+    """
     log = open('process.log', 'a')
     log.write('U_Audios\n')
     log.flush()
     print('Concatenating audios')
     
-    # Union of audios generated with Audio_festival()
-    # Necessary file for input to ffmpeg for audio union
-    
     f = open('Aud.txt', 'w')
+    
     for i in range(N):
+        
         f.write('file '+route_audio+'/0%d.wav \n'%i)
     f.close()
     command_line = 'ffmpeg -f concat -safe 0 -i Aud.txt -c copy ' + route_audio \
         + '/audio.wav'
     args = shlex.split(command_line)
-    #file_log=subprocess.Popen(args).communicate()
     c = subprocess.Popen(args, stdout=log, stderr=log)
 
 
-def Video_U_Audio(route_audio, route_video, name_video):
+def video_u_audio(route_audio, route_video, name_video):
+    """Join audio and video.
     
-    #Concatenate video obtained in Image_Video() and audio obtained in  U_Audios().
-   
+    Args:
+    route_audio (str): Route audio.
+    route_video (str): Rote video.
+    name_video (str): Output video name.
+    
+    Returns:
+    video (file): Video. 
+    
+    """
     log = open('process.log', 'a')
     log.write('Video_U_Audio\n')
     log.flush() 
@@ -50,7 +74,6 @@ def Video_U_Audio(route_audio, route_video, name_video):
     command_line = 'ffmpeg -i '+route_video+'/Video0.mp4 -i '+route_audio+'/audio.wav -strict -2 '+route_video+'/'+name_video
     args = shlex.split(command_line)
     c = subprocess.Popen(args,stdout=log, stderr=log).communicate()
-    
 
 
 

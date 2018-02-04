064f3baf018a36c01cafac6fad4777e154dc9a15
manuela98 <manujarare@hotmail.com>
2018-02-04 11:01:37 -0500

    Adding documentation

--- talkscript2media/storage.py
+++ talkscript2media/storage.py
@@ -1,56 +1,63 @@
-#storage
-
 import os
 
-def Storage(route_images,route_text,route_audio,route_video):
 
+def store(route_images,route_text,route_audio,route_video):
+   
+    """Verify the existence of directories.
     
-    #image storage path ,text storage path,video output path  
-    
+    Using the routes provided by the file, check the routes and create 
+    the audio storage directory.
+
+    Args:
+        route_images (srt): Image directory path.
+        route_text (str): Text directory path.
+        route_audio (str): Audio directory path.
+        route_video (str): Video directory path.
+        
+    Returns:
+        Output in console warning non-existence.
+        
+    """
     if not os.path.exists(route_images) or not os.path.exists(route_text) or not os.path.exists(route_video):
-        print('the routes do not exist')
+        print('Some of the routes do not exist')
     
     else: 
-     #if they extend, a route is created to store the audio
-       if not os.path.exists(route_audio): os.makedirs(route_audio)
+        if not os.path.exists(route_audio): os.makedirs(route_audio)
+        
             
-    return 0
-            
-
-
-
-
-def Existence(file):
     
-    #Verifying the existence of each file (images and text) specified and extracts information from file paths
+def existence(file):
+    """Extracts information from file paths.
+    
+    Args:
+    file (file): File with specific format to generate the video.
+    
+    Returns:
+    img (str): Name of each image included in the video in order.
+    text (str): Name of each text file included corresponding to each image.
+    language (str): Voice for festival.
+    routes (str): Name of routes.
+    
+    """
     f = open(file,'r')
     file = f.readlines()
     
     language = file[0].split(' \n')[0]
-
     
-    
-    #stores names of img
     img = [] 
     text = []
     
-    file_1 = file[1:file.index('\n')] #list image and text information
+    file_1 = file[1:file.index('\n')] 
     file_2 = file[file.index('\n')+1:]
-
     
-    #get image name and text name
-    
-    img = [i.split('>')[0] for i in file_1]
+    # Get image name and text name   
+    img = [i.split('>')[0] for i in file_1]  
     text = [i.split('>')[1].split('\n')[0] for i in file_1]
     
-    #get routes specified in the file
-    
+    # Get routes specified in the file
     routes = [i.split('=')[1].split('\n')[0] for i in file_2]  
     
     return img,text,language,routes
-    
-
-
         
         
         

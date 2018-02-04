19c6675858122405295ff8a6a3f99b0e999292ed
manuela98 <manujarare@hotmail.com>
2018-02-04 11:17:10 -0500

    Function names with uppercase to lowercase

--- talkscript2media/__main__.py
+++ talkscript2media/__main__.py
@@ -3,7 +3,7 @@ import talkscript2media.text_to_audio as text_to_audio
 import talkscript2media.to_video as to_video
 
 # Exist[0] Name images, Exist[1] Name texts, Exist[2] language,Exist[3] routes.
-Exist = storage.Existence('tests/file.txt')
+Exist = storage.existence('tests/file.txt')
 
 
 
@@ -15,16 +15,16 @@ route_images = Exist[3][3]
 route_text = Exist[3][4]
 route_audio = Exist[3][5]
 
-N = text_to_audio.Number_images(route_images)
+N = text_to_audio.number_images(route_images)
 
-storage.Storage(route_images, route_text, route_audio, route_output_video)
-
-
+storage.store(route_images, route_text, route_audio, route_output_video)
 
 
-text_to_audio.Audio_festival(route_text, route_audio, Exist[1], Exist[2], N)
-text_to_audio.Duration(N,route_images, route_audio, Exist[0])
 
-to_video.Image_Video(route_output_video)
-to_video.U_Audios(N,route_audio)
-to_video.Video_U_Audio(route_audio, route_output_video, name_output_video)
+
+text_to_audio.audio_festival(route_text, route_audio, Exist[1], Exist[2], N)
+text_to_audio.duration(N,route_images, route_audio, Exist[0])
+
+to_video.image_video(route_output_video)
+to_video.u_audios(N,route_audio)
+to_video.video_u_audio(route_audio, route_output_video, name_output_video)
